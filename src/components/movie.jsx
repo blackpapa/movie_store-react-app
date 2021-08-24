d563b7491cce08658d3670f19815a2e4b30a60da
Andy Chen <andycjx8@gmail.com>
2021-08-24 10:29:03 +1000

    Add key to each childs in the tr

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -18,7 +18,7 @@ class Movie extends Component {
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
