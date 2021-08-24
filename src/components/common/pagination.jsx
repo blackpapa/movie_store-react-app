5847b31dcac94baeb4b4a12680f9abbe981cb2cc
Andy Chen <andycjx8@gmail.com>
2021-08-24 15:17:33 +1000

    Create an array to store pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import _ from "lodash";
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange } = props;
+  //[1, 2, 3]
+  const page = Math.ceil(itemsCount / pageSize);
+  const pages = _.range(1, page + 1);
+  console.log(pages);
   return <h1>Pagination</h1>;
 };
 
