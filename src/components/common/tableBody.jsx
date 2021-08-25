194e4f5d4cf9f81856d9439dce182e260be0c923
Andy Chen <andycjx8@gmail.com>
2021-08-25 14:12:20 +1000

    Change to class component and add rendercell fc

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import _ from "lodash";
 
-const TableBody = (props) => {
-  const { items, columns } = props;
+class TableBody extends Component {
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
 
-  return (
-    <tbody>
-      {items.map((item) => (
-        <tr key={item._id}>
-          {columns.map((column) => (
-            <td>{_.get(item, column.path) || column.content(item)}</td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-  );
-};
+    return _.get(item, column.path);
+  };
+
+  render() {
+    const { items, columns } = this.props;
+    return (
+      <tbody>
+        {items.map((item) => (
+          <tr key={item._id}>
+            {columns.map((column) => (
+              <td>{this.renderCell(item, column)}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
 
 export default TableBody;
