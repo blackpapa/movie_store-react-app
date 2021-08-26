6332413fc6003e148bddde6745a58e18c610857f
Andy Chen <andycjx8@gmail.com>
2021-08-27 08:54:50 +1000

    Set up login from html mark up

---
+++ src/components/loginForn.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {};
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input type="text" id="username" className="form-control" />
+        </div>
+        <div>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input type="text" id="password" className="form-control" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
