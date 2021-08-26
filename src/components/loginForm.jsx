ea7e9656dba854368be74708d4ddaf3c812356b5
Andy Chen <andycjx8@gmail.com>
2021-08-27 09:19:22 +1000

    Bind input state with react state

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  state = {};
+  state = { account: { username: "", password: "" } };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -14,13 +14,24 @@ class LoginForm extends Component {
           <label htmlFor="username" className="form-label">
             Username
           </label>
-          <input autoFocus type="text" id="username" className="form-control" />
+          <input
+            autoFocus
+            value={this.state.account.username}
+            type="text"
+            id="username"
+            className="form-control"
+          />
         </div>
         <div>
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="text" id="password" className="form-control" />
+          <input
+            value={this.state.account.password}
+            type="text"
+            id="password"
+            className="form-control"
+          />
         </div>
         <button className="btn mt-3 w-100 btn-primary">Login</button>
       </form>
