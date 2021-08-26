3b5a2373b183af19c681ad7a95c80d181472143c
Andy Chen <andycjx8@gmail.com>
2021-08-26 14:33:50 +1000

    Build fake customers

---
+++ src/services/fakeCustomerService.js
@@ -0,0 +1,9 @@
+const customers = [
+  { _id: "jfsdf342342f", name: "Andy", isGold: true, phone: "32344545" },
+  { _id: "jfsdf342342e", name: "Janelle", isGold: true, phone: "98932343" },
+  { _id: "jfsdf3423fds", name: "Jack", isGold: false, phone: "32344545" },
+];
+
+export function getCustomers() {
+  return customers;
+}
