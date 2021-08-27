390f936093ef26d8830e97d423abfcc86b2d1a0d
Andy Chen <andycjx8@gmail.com>
2021-08-27 13:50:01 +1000

    Implement validating basic input fields

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -15,7 +15,7 @@ const Input = (props) => {
         id={name}
         className="form-control"
       />
-      {error && <div className="alert alert-danger">{error}</div>}
+      {error && <div className="alert  alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -11,8 +11,12 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
+
     this.setState({ errors });
+    if (errors) return;
+
+    //call the server
+    console.log("submitted");
   };
 
   validate = () => {
@@ -28,14 +32,29 @@ class LoginForm extends Component {
     return Object.keys(errors) === 0 ? null : errors;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+    }
+  };
+
   handleChange = (e) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(e.currentTarget);
+    if (errorMessage) errors[e.currentTarget.name] = errorMessage;
+    else delete errors[e.currentTarget.name];
+
     const account = { ...this.state.account };
     account[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <form onSubmit={this.handleSubmit} className="form-login">
         <Input
@@ -43,12 +62,14 @@ class LoginForm extends Component {
           name="username"
           value={account.username}
           onChange={this.handleChange}
+          error={errors.username}
         />
         <Input
           label="Password"
           name="password"
           value={account.password}
           onChange={this.handleChange}
+          error={errors.password}
         />
         <button className="btn mt-3 w-100 btn-primary">Login</button>
         <p className="mt-5 mb-3 text-muted text-center">© 2017–2021</p>
