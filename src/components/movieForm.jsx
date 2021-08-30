1a8a54109e926a4310f97da2a3c89fd317a0cf57
Andy Chen <andycjx8@gmail.com>
2021-08-30 10:32:53 +1000

    Create select menu mark up

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { getGenres } from "../services/fakeGenreService";
 import Form from "./common/form";
 
 class MovieForm extends Form {
+  genres = getGenres();
+
   doSubmit = () => {
     this.props.history.push("/");
   };
@@ -10,9 +13,18 @@ class MovieForm extends Form {
     const { match } = this.props;
     return (
       <div>
-        <form action="" className="form-login">
+        <form className="form-login">
           <h1>{match.params.id}</h1>
           {this.renderInput("title", "Title")}
+          <label htmlFor="genres" className="form-label">
+            Genre
+          </label>
+          <select name="genres" id="genres" className="custom-select">
+            <option selected>Action</option>
+            {this.genres.map((g) => (
+              <option>{g.name}</option>
+            ))}
+          </select>
           {this.renderInput("numberInStock", "Number in Stock")}
           {this.renderInput("dailyRentalRate", "Rate")}
           <button onClick={this.handleSave} className="button btn btn-primary">
