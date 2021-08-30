2319ae147ee1b7fd876afd3463a12395d3b63e87
Andy Chen <andycjx8@gmail.com>
2021-08-30 16:24:28 +1000

    Add type, name to input

--- src/components/common/searchBox.jsx
+++ src/components/common/searchBox.jsx
@@ -3,6 +3,8 @@ import React from "react";
 const SearchBox = ({ value, onChange }) => {
   return (
     <input
+      type="text"
+      name="query"
       className="form-control my-3"
       placeholder="Search..."
       value={value}
