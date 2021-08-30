d078fe601e7859077c683512b213d58f8fdcff32
Andy Chen <andycjx8@gmail.com>
2021-08-30 10:44:27 +1000

    Let select styled with form-control

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Select = ({ name, label, items }) => {
   return (
-    <div>
+    <div className="form-group">
       <label htmlFor={name} className="form-label">
         {label}
       </label>
-      <select name={name} id={name} className="custom-select">
-        <option selected>Default</option>
+      <select name={name} id={name} className="form-control">
+        <option value="">Default</option>
         {items.map((item) => (
-          <option>{item.name}</option>
+          <option key={item._id} value={item._id}>
+            {item.name}
+          </option>
         ))}
       </select>
     </div>
