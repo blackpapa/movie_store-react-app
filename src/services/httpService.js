67a9c3fe8c2983e8b567b31c8bf5dedb53126680
Andy Chen <andycjx8@gmail.com>
2021-09-02 11:24:18 +1000

    Implement axios interceptros for unex errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,4 +1,18 @@
 import axios from "axios";
+import logger from "./logService";
+import { toast } from "react-toastify";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status <= 500;
+
+  if (!expectedError) {
+    toast.error("Unexpected occured !");
+    logger.log(error);
+  }
+});
 
 export default {
   get: axios.get,
