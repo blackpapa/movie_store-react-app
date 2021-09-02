8feb5e0be4725777a10acbf91dc3d8b07f3f5f5c
Andy Chen <andycjx8@gmail.com>
2021-09-02 11:19:14 +1000

    Create log service

---
+++ src/services/logService.js
@@ -0,0 +1,23 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://d76fd96adda8459dadf07249de42b5a0@o494006.ingest.sentry.io/5938301",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
