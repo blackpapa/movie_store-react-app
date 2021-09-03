8ca3bc7fe60292c12f914e7d8398c034391fe614
Andy Chen <andycjx8@gmail.com>
2021-09-03 11:47:05 +1000

    Display user on nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
@@ -10,14 +11,26 @@ import NavBar from "./components/common/navbar";
 import Footer from "./components/common/footer";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
-import { ToastContainer } from "react-toastify";
+import "./App.css";
 
 class App extends Component {
+  state = { user: {} };
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (error) {
+      //If jwt is null, pass the error
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             <Route path="/movies/:id" component={MovieForm}></Route>
