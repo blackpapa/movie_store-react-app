87f6f2064648dc811a45178b9eab5d571394a84f
Andy Chen <andycjx8@gmail.com>
2021-09-06 10:04:43 +1000

    Render method for route

--- src/App.js
+++ src/App.js
@@ -23,17 +23,21 @@ class App extends Component {
   }
 
   render() {
+    const { user } = this.state;
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar user={this.state.user} />
+        <NavBar user={user} />
         <main className="container">
           <Switch>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
             <Route path="/not-found" component={NotFound}></Route>
-            <Route path="/movies" component={Movies}></Route>
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={user} />}
+            ></Route>
             <Route path="/login" component={LoginForm}></Route>
             <Route path="/logout" component={Logout}></Route>
             <Route path="/register" component={RegisterForm}></Route>
