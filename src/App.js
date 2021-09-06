043cae1a52b3146ace534d5a63569a71c6d4866c
Andy Chen <andycjx8@gmail.com>
2021-09-06 10:12:31 +1000

    Create protected route for login page

--- src/App.js
+++ src/App.js
@@ -30,14 +30,20 @@ class App extends Component {
         <NavBar user={user} />
         <main className="container">
           <Switch>
-            <Route path="/movies/:id" component={MovieForm}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound}></Route>
+            <Route
+              path="/movies/:id"
+              render={(props) => {
+                if (!user) return <Redirect to="/login" />;
+                return <MovieForm {...props} />;
+              }}
+            ></Route>
             <Route
               path="/movies"
               render={(props) => <Movies {...props} user={user} />}
             ></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
             <Route path="/login" component={LoginForm}></Route>
             <Route path="/logout" component={Logout}></Route>
             <Route path="/register" component={RegisterForm}></Route>
