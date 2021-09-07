6aa121d94c49fa3ecde4c2c1bea9445212cbaac9
Andy Chen <andycjx8@gmail.com>
2021-09-07 16:37:13 +1000

    Clear code

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import { Component } from "react";
+import { Link } from "react-router-dom";
 import { deleteMovie, getMovies } from "../services/movieService";
 import { getGenres } from "../services/genreService";
+import { paginate } from "./utils/paginate";
+import { toast } from "react-toastify";
 import Pagination from "./common/pagination";
 import Listgroup from "./common/listgroup";
-import { paginate } from "./utils/paginate";
 import MovieTable from "./movieTable";
-import { Link } from "react-router-dom";
-import _ from "lodash";
 import SearchBox from "./common/searchBox";
-import { toast } from "react-toastify";
+import _ from "lodash";
 
 interface Movie {
   _id: string,
