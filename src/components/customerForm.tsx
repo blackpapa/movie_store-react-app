05ed18bc204d246d71cd86709649615ce6a9b26b
Andy Chen <andycjx8@gmail.com>
2021-09-11 10:18:10 +1000

    Set up schema and html markup

--- src/components/customerForm.tsx
+++ src/components/customerForm.tsx
@@ -1,11 +1,29 @@
+import Joi from 'joi';
 import React from 'react';
 import Form from './common/form';
 
  
 class CustomerFrom extends Form {
-    state = {data: {}, errors: {} }
+    state = {data: {name: "", isGold: "", phone: ""}, errors: {} }
+    
+    schemaObj = {
+        _id: Joi.string(),
+        name: Joi.string().required(),
+        isGold: Joi.boolean(),
+        phone: Joi.string().required()
+    }
+    schema = Joi.object(this.schemaObj)
+
     render() { 
-        return (  <h1>{this.props.match.params.id}</h1>);
+        return (<div>
+            <form className="form-login">
+                <h1>{this.props.match.params.id}</h1>
+                {this.renderInput('name', 'Name')}
+                {this.renderInput('isGold', 'IsGold')}
+                {this.renderInput('phone', 'Phone')}
+                {this.renderButton('Save')}
+            </form>
+        </div>);
     }
 }
  
