afe4613f22fd26e2f9eb80e460e4489e27c23e15
Andy Chen <andycjx8@gmail.com>
2021-09-14 08:45:25 +1000

    Define interface user

--- src/components/customers.tsx
+++ src/components/customers.tsx
@@ -1,42 +1,47 @@
 import React, { Component } from "react";
 import { getCustomers } from "../services/customerService";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import CustomerTable from "./customerTable";
 import SearchBox from "./common/searchBox";
 import _ from "lodash";
 import ProgressBar from "./common/progressBar";
-import { deleteCustomer } from './../services/customerService';
+import { deleteCustomer } from "./../services/customerService";
 import { toast } from "react-toastify";
+import { User } from "./common/navbar";
 
 export interface Customer {
-  _id:string,
-  name: string,
-  phone: string,
-  isGold?: boolean
+  _id: string;
+  name: string;
+  phone: string;
+  isGold?: boolean;
 }
 
 interface SortColumn {
-  path: string,
-  order: string,
+  path: string;
+  order: string;
 }
 
 interface State {
-  customers: Customer[],
-  sortColumn: SortColumn,
-  searchQuery: string,
-  loadCompleted: boolean
+  customers: Customer[];
+  sortColumn: SortColumn;
+  searchQuery: string;
+  loadCompleted: boolean;
 }
 
-class Customers extends Component<{},State> {
+interface Props {
+  user?: User;
+}
+
+class Customers extends Component<Props, State> {
   state = {
     customers: [],
     sortColumn: { path: "name", order: "asc" },
     searchQuery: "",
-    loadCompleted: false
+    loadCompleted: false,
   };
 
   async componentDidMount() {
-    const {data: customers} = await getCustomers();
+    const { data: customers } = await getCustomers();
     this.setState({ customers, loadCompleted: true });
   }
 
@@ -48,7 +53,7 @@ class Customers extends Component<{},State> {
     this.setState({ searchQuery });
   };
 
-  handleDelete = async (customer:Customer) => {
+  handleDelete = async (customer: Customer) => {
     const originalCustomers = this.state.customers;
     const customers = originalCustomers.filter(
       (c: Customer) => c._id !== customer._id
@@ -56,7 +61,7 @@ class Customers extends Component<{},State> {
     this.setState({ customers });
 
     try {
-      await deleteCustomer(customer._id)
+      await deleteCustomer(customer._id);
     } catch (error: any) {
       if (error.response && error.response.status === 404) {
         toast.error("The movie has already been deleted ");
@@ -65,9 +70,8 @@ class Customers extends Component<{},State> {
       } else if (error.response) {
         toast.error(error.response.data);
       }
-      this.setState({customers: originalCustomers})
+      this.setState({ customers: originalCustomers });
     }
-
   };
 
   getPageData = () => {
@@ -80,31 +84,39 @@ class Customers extends Component<{},State> {
         c.name.toLowerCase().startsWith(searchQuery.toLowerCase())
       );
     }
-    customers = _.orderBy(customers, sortColumn.path, sortColumn.order as ("asc" | "desc"));
+    customers = _.orderBy(
+      customers,
+      sortColumn.path,
+      sortColumn.order as "asc" | "desc"
+    );
 
-    return {customers, totalCount: customers.length}
-  }
+    return { customers, totalCount: customers.length };
+  };
 
   render() {
-    const { sortColumn, searchQuery, loadCompleted} = this.state;
+    const { sortColumn, searchQuery, loadCompleted } = this.state;
 
-    const {customers, totalCount} = this.getPageData()
+    const { customers, totalCount } = this.getPageData();
 
     return (
       <React.Fragment>
-        {!loadCompleted? <ProgressBar />: <div>
-        <Link to ="/customers/new">
-          <button className="btn btn-primary">New Customer</button>
-        </Link>
-        <SearchBox value={searchQuery} onChange={this.handleSearch} />
-        <p>There are {totalCount} customers in the store</p>
-        <CustomerTable
-          customers={customers}
-          onSort={this.handleSort}
-          onDelete={this.handleDelete}
-          sortColumn={sortColumn}
-        /></div>}
-        
+        {!loadCompleted ? (
+          <ProgressBar />
+        ) : (
+          <div>
+            <Link to="/customers/new">
+              <button className="btn btn-primary">New Customer</button>
+            </Link>
+            <SearchBox value={searchQuery} onChange={this.handleSearch} />
+            <p>There are {totalCount} customers in the store</p>
+            <CustomerTable
+              customers={customers}
+              onSort={this.handleSort}
+              onDelete={this.handleDelete}
+              sortColumn={sortColumn}
+            />
+          </div>
+        )}
       </React.Fragment>
     );
   }
