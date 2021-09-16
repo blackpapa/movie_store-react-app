5a9f3909169a9c9a474cc7077acce451b1b7a29a
Andy Chen <andycjx8@gmail.com>
2021-09-16 14:28:14 +1000

    Add composewithdevtools

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import rootReducer from './Redux/Reducers/rootReducer';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store
