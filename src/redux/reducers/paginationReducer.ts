ec78ef9a1edc74d438409e6f8103dd0de3d6c359
Andy Chen <andycjx8@gmail.com>
2021-09-16 14:22:48 +1000

    Set up rootreducer and page reducer

---
+++ src/redux/reducers/paginationReducer.ts
@@ -0,0 +1,12 @@
+const initialState =  {
+    pageSize: 5,
+    currentPage: 1
+}
+
+const paginationReducer = (state = initialState, action) => {
+
+    return state;
+
+}
+
+export default paginationReducer
---
+++ src/redux/reducers/rootReducer.ts
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux'
+import paginationReducer from './paginationReducer'
+
+
+const rootReducer = combineReducers({
+    pagination: paginationReducer
+})
+
+export default rootReducer
