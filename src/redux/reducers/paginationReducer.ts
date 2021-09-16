8414831ec0d693ba2d1c57778e0e3d29edfa0a8d
Andy Chen <andycjx8@gmail.com>
2021-09-16 14:39:07 +1000

    Implement paginationReducer

--- src/redux/reducers/paginationReducer.ts
+++ src/redux/reducers/paginationReducer.ts
@@ -1,9 +1,20 @@
+import { SET_CURRENTPAGE } from "../actions";
+
 const initialState =  {
     pageSize: 5,
     currentPage: 1
 }
 
 const paginationReducer = (state = initialState, action: any) => {
+    const {type, payload} = action
+
+    switch(type) {
+        case SET_CURRENTPAGE:
+            return {
+                ...state,
+                currentpage: payload
+            }
+    }
 
     return state;
 
