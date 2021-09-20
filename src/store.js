05796706480558a8f5cb4bdaf58feeef666623f7
Andy Chen <andycjx8@gmail.com>
2021-09-20 14:14:15 +1000

    Clear store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,7 @@
-import { applyMiddleware, createStore } from "redux";
+import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk),
-  composeWithDevTools()
-);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
