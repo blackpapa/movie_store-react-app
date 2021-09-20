4e644619cf45d7c396026cf4d397aca8c82eb13e
Andy Chen <andycjx8@gmail.com>
2021-09-20 15:03:43 +1000

    Update name to chart

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -23,7 +23,7 @@ const Chart: React.FC<ChartProps> = () => {
   return (
     <div>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-1 pb-2 mb-3 border-bottom">
-        <h1 className="h2">Dashboard</h1>
+        <h1 className="h2">Chart</h1>
         <BtnToolBar />
       </div>
       {state.loading ? (
