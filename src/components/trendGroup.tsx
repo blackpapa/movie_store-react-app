2e23589cc3e5f3248729d8c4a710ed3a10c2d64c
Andy Chen <andycjx8@gmail.com>
2021-09-27 15:52:48 +1000

    Implement html markup

--- src/components/trendGroup.tsx
+++ src/components/trendGroup.tsx
@@ -1,22 +1,54 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, QueryResult, useQuery } from "@apollo/client";
+import _ from "lodash";
+import ProgressBar from "./common/progressBar";
+
+interface Movie {
+  id: string;
+  title: string;
+  rentalsCount: number;
+}
+
+interface MovieTrendData {
+  movies: Movie[];
+}
 
 interface TrendGroupProps {}
 
-const TrendGroup: React.FC<TrendGroupProps> = () => {
-  const MOVIES_TREND = gql`
-    {
-      movies {
-        title
-        rentalsCount
-      }
+const MOVIES_TREND = gql`
+  {
+    movies {
+      id
+      title
+      rentalsCount
     }
-  `;
+  }
+`;
 
-  const result = useQuery(MOVIES_TREND);
-  console.log(result);
+const TrendGroup: React.FC<TrendGroupProps> = () => {
+  const { data, error, loading }: QueryResult =
+    useQuery<MovieTrendData>(MOVIES_TREND);
+  if (loading) return <ProgressBar />;
+  if (error) return <h1>Error: {error}</h1>;
 
-  return <h1>Trend group</h1>;
+  const trends = _.orderBy(data.movies, "rentalsCount", "desc").slice(0, 5);
+
+  return (
+    <div>
+      <h5>Trend</h5>
+      <ul className="list-group">
+        {trends.map((trend) => (
+          <li
+            className="list-group-item d-flex justify-content-between align-items-center"
+            key={trend.id}
+          >
+            {`#${trend.title}`}
+            <small>{trend.rentalsCount}</small>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TrendGroup;
