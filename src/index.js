fe83b5893c7aa5c9cff104ebbe895f808ceac488
Andy Chen <andycjx8@gmail.com>
2021-09-27 14:07:24 +1000

    Set up apollo client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -9,6 +10,11 @@ import "react-toastify/dist/ReactToastify.css";
 import logger from "./services/logService";
 
 logger.init();
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL + "/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
