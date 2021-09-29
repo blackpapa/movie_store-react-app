ccaa421b2bbf46be8c23b5eebaa6415ce5ce5114
Andy Chen <andycjx8@gmail.com>
2021-09-30 08:39:20 +1000

    Ajax call from parent components

--- src/components/gqlCustomers.tsx
+++ src/components/gqlCustomers.tsx
@@ -2,13 +2,31 @@ import React from "react";
 import Customers from "../components/customers";
 import { User } from "./common/navbar";
 import { RouteComponentProps } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import ProgressBar from "./common/progressBar";
 
 interface gqlCustomersProps extends RouteComponentProps {
   user?: User;
 }
 
+const getCustomersQuery = gql`
+  {
+    customers {
+      _id
+      name
+      isGold
+      phone
+    }
+  }
+`;
+
 const GqlCustomers: React.FC<gqlCustomersProps> = ({ user, ...rest }) => {
-  return <Customers {...rest} user={user} />;
+  const { data, error, loading } = useQuery(getCustomersQuery);
+
+  if (loading) return <ProgressBar />;
+  if (error) return <h1>error</h1>;
+
+  return <Customers {...rest} user={user} customers={data.customers} />;
 };
 
 export default GqlCustomers;
